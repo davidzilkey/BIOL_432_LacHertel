bff8f576d64995c0743508fac88698f783517dd4
David R. Zilkey <d.zilkey@gmail.com>
2024-09-30 08:51:20 -0400

    updated scripts and made plots

--- 01_scripts/plot_2024.R
+++ 01_scripts/plot_2024.R
@@ -17,17 +17,18 @@ fluoro_conc_avg <- fluoro_conc %>%
 
 #Plot chla and cyano and arrange an output plot
 chla <- ggplot(data = fluoro_conc_avg, aes(x = chla_mean, y = depth_m)) +
-  geom_path() +
-  geom_point() +
-  geom_linerange(aes(xmin = chla_mean - chla_sd, xmax = chla_mean + chla_sd)) +
+  geom_path(colour = 'forestgreen') +
+  geom_point(colour = 'forestgreen') +
+  geom_linerange(aes(xmin = chla_mean - chla_sd, xmax = chla_mean + chla_sd), colour = 'forestgreen') +
   scale_y_reverse() +
-  labs(x = 'Chlorophyll a concentration (mg/L)', y = 'Depth (m)')
+  labs(x = 'Chlorophyll a (mg/L)', y = 'Depth (m)')
 
 cyano <- ggplot(data = fluoro_conc_avg, aes(x = cyano_mean, y = depth_m)) +
-  geom_path() +
-  geom_point() +
-  geom_linerange(aes(xmin = cyano_mean - cyano_sd, xmax = cyano_mean + cyano_sd)) +
+  geom_path(colour = 'blue') +
+  geom_point(colour = 'blue') +
+  geom_linerange(aes(xmin = cyano_mean - cyano_sd, xmax = cyano_mean + cyano_sd), colour = 'blue') +
   scale_y_reverse() +
-  labs(x = 'Cyanobacteria concentration (ug/L)', y = '')
+  labs(x = 'Cyanobacteria (ug/L)', y = '')
 
-ggarrange(chla, cyano)
+ggarrange(chla, cyano) +
+  labs(title = 'Lac Hertel Pigment Concentrations - September 28, 2024')
